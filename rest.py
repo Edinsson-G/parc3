35defe216e8ebe1c8f594c4addc547202abb6235
Edinsson-G <edinssonfernandogutierrez@gmail.com>
2022-03-08 03:24:50 +0000

    terminado

--- rest.py
+++ rest.py
@@ -1,8 +1,32 @@
-#!/usr/bin/python3
-
-from flask import flask, request
+from flask import Flask, request
 app=Flask(__name__)
-
+userage = { 'Pedro': '20', 'Maria': '19'}
 @app.route('/')
 def index():
     return 'Server Works!'
+
+@app.route('/userage')
+def age():
+    return userage['Pedro']
+
+#agregar variables
+@app.route('/user/<username>')
+def show_user_age(username):
+    return 'Age: %s' % userage[username]
+
+#modificar o agregar elementos
+@app.route('/add/<string:nombre>/<string:edad>')
+def modificar(nombre,edad):
+    userage[nombre]=edad
+    return 'exito'
+
+#eliminar
+@app.route('/supr/<nombre>')
+def suprimir(nombre):
+    userage.pop(nombre)
+    return 'exito'
+
+#muestra la lista completa
+@app.route('/lista')
+def listar():
+    return userage
